c53249fd6111c0b9fbf9f5620e24b9e189aed4bc
Diegoslourenco <diego.lourenco15@gmail.com>
2020-11-22 18:24:08 -0300

    lista 7 alocacao de memoria

--- Lista de Exercícios 7 - Alocação Dinâmica/l7exercicio1.c
+++ Lista de Exercícios 7 - Alocação Dinâmica/l7exercicio1.c
@@ -1,6 +1,6 @@
 /*
-1)Faça um programa que leia um valor n e crie dinamicamente um vetor de n elementos
-e passe esse vetor para uma função que vai ler os elementos desse vetor. Depois, no
+1)Faça um programa que leia um valor n e crie dinamicamente um vetor de n elementos e 
+passe esse vetor para uma função que vai ler os elementos desse vetor. Depois, no
 programa principal, o vetor preenchido deve ser impresso. Além disso, antes de
 finalizar o programa, deve-se liberar a área de memória alocada.
 */
